9abc7b7d142d2b6834741f04dc96cb7b042b1bde
vbohnec21 <vbohnec21@student.foi.hr>
2023-05-28 21:12:54 +0200

    Dodan label za pretraživanje

--- Software/Kadrovska/FrmMyRequests.Designer.cs
+++ Software/Kadrovska/FrmMyRequests.Designer.cs
@@ -34,6 +34,7 @@
 			this.pnlDisplayAndButtons = new System.Windows.Forms.SplitContainer();
 			this.dgvZahtjevi = new System.Windows.Forms.DataGridView();
 			this.txtSearch = new System.Windows.Forms.TextBox();
+			this.lblSearch = new System.Windows.Forms.Label();
 			this.btnEdit = new System.Windows.Forms.Button();
 			this.btnErase = new System.Windows.Forms.Button();
 			this.pnlAddAndButtons = new System.Windows.Forms.SplitContainer();
@@ -44,12 +45,12 @@
 			this.lblAbsenseEnd = new System.Windows.Forms.Label();
 			this.lblAbsenseStart = new System.Windows.Forms.Label();
 			this.lblInputType = new System.Windows.Forms.Label();
-			this.cboType = new Kadrovska.components.DockableComboBox();
 			this.txtOpis = new System.Windows.Forms.TextBox();
 			this.btnReset = new System.Windows.Forms.Button();
 			this.btnSubmit = new System.Windows.Forms.Button();
 			this.cldStartAbsense = new System.Windows.Forms.MonthCalendar();
 			this.cldEndAbsense = new System.Windows.Forms.MonthCalendar();
+			this.cboType = new Kadrovska.components.DockableComboBox();
 			((System.ComponentModel.ISupportInitialize)(this.pnlBodyAndHeader)).BeginInit();
 			this.pnlBodyAndHeader.Panel1.SuspendLayout();
 			this.pnlBodyAndHeader.Panel2.SuspendLayout();
@@ -131,6 +132,7 @@
 			// 
 			this.pnlDisplayAndButtons.Panel1.Controls.Add(this.dgvZahtjevi);
 			this.pnlDisplayAndButtons.Panel1.Controls.Add(this.txtSearch);
+			this.pnlDisplayAndButtons.Panel1.Controls.Add(this.lblSearch);
 			// 
 			// pnlDisplayAndButtons.Panel2
 			// 
@@ -149,12 +151,12 @@
 			this.dgvZahtjevi.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
 			this.dgvZahtjevi.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
 			this.dgvZahtjevi.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.dgvZahtjevi.Location = new System.Drawing.Point(0, 20);
+			this.dgvZahtjevi.Location = new System.Drawing.Point(0, 33);
 			this.dgvZahtjevi.Name = "dgvZahtjevi";
 			this.dgvZahtjevi.ReadOnly = true;
 			this.dgvZahtjevi.RowHeadersVisible = false;
 			this.dgvZahtjevi.ShowEditingIcon = false;
-			this.dgvZahtjevi.Size = new System.Drawing.Size(835, 246);
+			this.dgvZahtjevi.Size = new System.Drawing.Size(835, 233);
 			this.dgvZahtjevi.TabIndex = 1;
 			this.dgvZahtjevi.CellFormatting += new System.Windows.Forms.DataGridViewCellFormattingEventHandler(this.dgvZahtjevi_CellFormatting);
 			this.dgvZahtjevi.CurrentCellChanged += new System.EventHandler(this.dgvZahtjevi_CurrentCellChanged);
@@ -162,11 +164,21 @@
 			// txtSearch
 			// 
 			this.txtSearch.Dock = System.Windows.Forms.DockStyle.Top;
-			this.txtSearch.Location = new System.Drawing.Point(0, 0);
+			this.txtSearch.Location = new System.Drawing.Point(0, 13);
 			this.txtSearch.Name = "txtSearch";
 			this.txtSearch.Size = new System.Drawing.Size(835, 20);
 			this.txtSearch.TabIndex = 0;
 			this.txtSearch.TextChanged += new System.EventHandler(this.txtSearch_TextChanged);
+			// 
+			// lblSearch
+			// 
+			this.lblSearch.AutoSize = true;
+			this.lblSearch.Dock = System.Windows.Forms.DockStyle.Top;
+			this.lblSearch.Location = new System.Drawing.Point(0, 0);
+			this.lblSearch.Name = "lblSearch";
+			this.lblSearch.Size = new System.Drawing.Size(71, 13);
+			this.lblSearch.TabIndex = 2;
+			this.lblSearch.Text = "Pretraživanje:";
 			// 
 			// btnEdit
 			// 
@@ -324,21 +336,6 @@
 			this.lblInputType.Text = "Vrsta Zahtjeva";
 			this.lblInputType.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
 			// 
-			// cboType
-			// 
-			this.cboType.DisplayMember = "m_strName";
-			this.cboType.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.cboType.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawVariable;
-			this.cboType.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-			this.cboType.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-			this.cboType.FormattingEnabled = true;
-			this.cboType.ItemHeight = 38;
-			this.cboType.Location = new System.Drawing.Point(2, 33);
-			this.cboType.Margin = new System.Windows.Forms.Padding(0);
-			this.cboType.Name = "cboType";
-			this.cboType.Size = new System.Drawing.Size(165, 44);
-			this.cboType.TabIndex = 4;
-			// 
 			// txtOpis
 			// 
 			this.txtOpis.Dock = System.Windows.Forms.DockStyle.Fill;
@@ -390,6 +387,21 @@
 			this.cldEndAbsense.Visible = false;
 			this.cldEndAbsense.DateSelected += new System.Windows.Forms.DateRangeEventHandler(this.cldEndAbsense_DateSelected);
 			this.cldEndAbsense.Leave += new System.EventHandler(this.cld_Leave);
+			// 
+			// cboType
+			// 
+			this.cboType.DisplayMember = "m_strName";
+			this.cboType.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.cboType.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawVariable;
+			this.cboType.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.cboType.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			this.cboType.FormattingEnabled = true;
+			this.cboType.ItemHeight = 38;
+			this.cboType.Location = new System.Drawing.Point(2, 33);
+			this.cboType.Margin = new System.Windows.Forms.Padding(0);
+			this.cboType.Name = "cboType";
+			this.cboType.Size = new System.Drawing.Size(165, 44);
+			this.cboType.TabIndex = 4;
 			// 
 			// FrmMyRequests
 			// 
@@ -450,5 +462,6 @@
 		private System.Windows.Forms.Button btnReset;
 		private System.Windows.Forms.DataGridView dgvZahtjevi;
 		private System.Windows.Forms.TextBox txtSearch;
+		private System.Windows.Forms.Label lblSearch;
 	}
 }
